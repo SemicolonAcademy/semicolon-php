3bd20f3500ba2f12d5cc2494df9b4b679bb74438
semicolondev <semicolondev@bf592c3c-58be-6ffc-397f-4f355295dc4e>
2011-04-25 05:47:31 +0000

    chapter three - functions added code with explanations

--- ch2_functions/substring.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-	echo substr("Hello world!", 1)."<br/>";
-	echo substr("Hello world!", 0)."<br/>";
-	echo substr("Hello world!", 50)."<br/>";
-	echo substr("Hello world!", 5, 4)."<br/>";	
-	
-?>
