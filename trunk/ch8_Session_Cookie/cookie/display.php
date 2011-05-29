17fde9303f6d85b46c0e1a19d7f3d4c8b8dc472b
semicolondev <semicolondev@bf592c3c-58be-6ffc-397f-4f355295dc4e>
2011-05-29 04:23:57 +0000

    Using cookie in PHP

---
+++ trunk/ch8_Session_Cookie/cookie/display.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_COOKIE['last_visit'])) $visit = $_COOKIE['last_visit']; 
+else echo "You've got some stale cookies!";
+
+echo "Your last visit was - ". $visit;
+?>
+<br/>
+<a href="destroy.php">Destroy cookie</a>
